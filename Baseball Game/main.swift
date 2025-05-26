bd5e8c1ef48054d05863dcecf044d7de887730be
Wooseong <thehousedone@gmail.com>
2025-05-26 15:05:31 +0900

    feat: 랜덤 번호 세개 뽑기, 인풋 받기 작성중 첫 커밋

--- Baseball Game/main.swift
+++ Baseball Game/main.swift
@@ -2,10 +2,45 @@
 //  main.swift
 //  Baseball Game
 //
-//  Created by 서광용 on 5/26/25.
+//  Created by 김우성 on 5/26/25.
 //
 
 import Foundation
 
-print("Hello, World!")
+let game = BaseballGame()
+game.start()
+
+class BaseballGame {
+    func start() {
+        print("< 게임을 시작합니다 >")
+        let input = getInput()
+        let answer = makeAnswer()
+        
+        
+    }
+    
+    func makeAnswer() -> [Int] {
+        let range = Array(1...9)
+        let randomNumbers = Array(range.shuffled().prefix(3))
+//        print(randomNumbers)
+        return randomNumbers
+    }
+    
+    func getInput() -> [Int] {
+        while true {
+            print("숫자를 입력하세요")
+            let input = readLine() ?? ""
+            
+            if input.count == 3 {
+                return input.compactMap { Int(String($0)) }
+            } else {
+                print("숫자를 3자리로 입력하세요")
+                
+            }
+        }
+    }
+}
+
+
+
 
